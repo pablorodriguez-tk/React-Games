5483ba69fe2b239719ed7313911885cbae08e09c
pablorodriguez-tk <pablorodriguez88@gmail.com>
2021-08-02 17:23:43 -0300

    Add search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { useAppContext } from '../../context/AppContext';
+import useForm from '../../hooks/useForm';
+import Game from '../Game/Game';
+import './searchBar.css';
 
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [formValues, handleInputChange] = useForm({ searchText: '' });
+  const { gameList } = useAppContext();
+  const { searchText } = formValues;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(searchText);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <div className="icon-input">
+          <input
+            type="text"
+            placeholder="Search"
+            className="form-control"
+            value={searchText}
+            onChange={handleInputChange}
+            name="searchText"
+            autoComplete="off"
+            aria-describedby="addon-wrapping"
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
